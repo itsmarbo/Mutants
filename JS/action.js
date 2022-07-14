adb091e6284e16e5a6726543ee703846e04fd751
Rony J. Letona <zronyj@gmail.com>
2022-07-14 15:25:35 +0200

    Adding text input
    Supporting text input.
    v-model two way binding not working because the range input is "constantly changing"

--- JS/action.js
+++ JS/action.js
@@ -36,13 +36,30 @@ const { createApp } = Vue
     },
     methods: {
       updateAA() {
-        this.mutCode = "Mutation: " + this.aaSequence[this.aa] + (+this.aa + 1) + this.sel;
+        this.mutCode = this.aaSequence[this.aa] + (+this.aa + 1) + this.sel;
       },
       onAA(nam) {
         this.image2 = this.aaMut[nam][1];
         this.sel = nam;
+        //this.mutCode = this.aaSequence[this.aa] + (+this.aa + 1) + nam;
       },
       run() {
+        if ((this.mutCode.length >= 3) && (this.mutCode.length <= 5)) {
+          var primera = this.mutCode.slice(0,1);
+          var ultima = this.mutCode.slice(-1);
+          if (Object.keys(this.aaMut).includes(primera) && Object.keys(this.aaMut).includes(ultima)) {
+            var numero = this.mutCode.slice(1,-2);
+            if ((+numero >= 1) && (+numero < this.aaSequence.length)) {
+              console.log("Genial!")
+            } else {
+              console.log("Error de numero");
+            }
+          } else {
+            console.log("Error de letras");
+          }
+        } else {
+          console.log("Error de largo");
+        }
         this.third = true;
         this.isDisabled = true;
       }
