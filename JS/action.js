1d1fc0e8d51d6f6702f3c10fb7a9812661e32e42
Rony J. Letona <zronyj@gmail.com>
2022-07-24 13:48:38 -0600

    Commenting ...
    Adding some comments both in the HTML and in the JS portions to make it clear.

--- JS/action.js
+++ JS/action.js
@@ -1,12 +1,14 @@
 const { createApp } = Vue
   createApp({
     data() {
+      /* Seccion de variables */
       return {
-        primero: false,
-        third: false,
-        fourth: false,
-        isDisabled: false,
-        aaSequence: ["G"],
+        primero: false, // Control para mostrar la secuencia
+        third: false, // Control para mostrar el spinner
+        fourth: false, // Control para mostrar resultados
+        isDisabled: false, // Para desactivar los controles para seleccionar AA
+        aaSequence: ["G"], // Secuencia de AA de la proteina (se carga con Axios al iniciar la pagina)
+        // Objeto con AAs: llaves - letra, valor - nombre e imagen
         aaMut: {"G" : ["Glycine", "glycine.png"],
                 "A" : ["Alanine", "alanine.png"],
                 "L" : ["Leucine", "leucine.png"],
@@ -28,24 +30,27 @@ const { createApp } = Vue
                 "D" : ["Aspartic Acid", "aspartic_acid.png"],
                 "T" : ["Threonine", "threonine.png"]
               },
-        imgs: {},
-        image1: "glycine.png",
-        image2: "methionine.png",
-        mutCode: "",
-        aa: 0,
-        sel: "M",
-        endpoint: "http://127.0.0.1:5000/"
+        imgs: {}, // Objeto para pre-cargar las imagenes
+        image1: "glycine.png", // Imagen 1
+        image2: "methionine.png", // Imagen 2
+        mutCode: "", // Codigo de la mutacion
+        aa: 0, // Posicion del AA a mutar
+        sel: "M", // Letra del AA que reemplazara
+        endpoint: "http://127.0.0.1:5000/" // URL del backend
       }
     },
     methods: {
+      /* Metodo para actualizar el codigo de mutacion al mover el slider */
       updateAA() {
         this.mutCode = this.aaSequence[this.aa] + (+this.aa + 1) + this.sel;
       },
+      /* Metodo para actualizar el codigo de mutacion al seleccionar el AA para reemplazar */
       onAA(nam) {
         this.image2 = this.aaMut[nam][1];
         this.sel = nam;
         this.mutCode = this.aaSequence[this.aa] + (+this.aa + 1) + nam;
       },
+      /* Metodo para pedirle la evaluacion de la nueva secuencia segun el modelo al backend */
       run() {
         if ((this.mutCode.length >= 3) && (this.mutCode.length <= 5)) {
           var primera = this.mutCode.slice(0,1);
@@ -77,7 +82,9 @@ const { createApp } = Vue
         this.isDisabled = true;
       }
     },
+    /* !!!!! ANTES DE MONTAR VUE.JS EN LA PAGINA !!!!! */
     beforeMount () {
+      /* Solicitar al backend la secuencia de AAs */
       const getSequence = async () => {
         await axios
           .get(this.endpoint + "sequence")
@@ -88,7 +95,9 @@ const { createApp } = Vue
       }
       getSequence();
     },
+    /* !!!!! JUSTO DESPUES DE MONTAR VUE.JS EN LA PAGINA !!!!! */
     mounted () {
+      /* Pre-cargar las imagenes en el cache del navegador para no hacer el proceso tan demandante */
       Object.keys(this.aaMut).forEach(element => {
         this.imgs[element] = new Image();
         this.imgs[element].src = "IMG/" + this.aaMut[element][1];
