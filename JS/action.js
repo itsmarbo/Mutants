923ca6892b31641f2a839ea53a5f9d89ca15511d
Rony J. Letona <zronyj@gmail.com>
2022-08-05 00:58:08 -0600

    Revert "Keyboards"
    This reverts commit 6debc235f6dede81dbeef884aa8ab58b9989e857, reversing
    changes made to d1aa094822ad0106d9be9babf315b6cfada1eff2.

--- JS/action.js
+++ JS/action.js
@@ -1,20 +1,11 @@
 const { createApp } = Vue
   createApp({
     data() {
-      /* Seccion de variables */
       return {
-        endpoint: "http://127.0.0.1:5000/", // URL del backend
-        seqMode: true, // Seleccionar AA o ADN
-        primero: false, // Control para mostrar la secuencia de AA
-        segundo: false, // Control para mostrar la secuencia de ADN
-        third: false, // Control para mostrar el spinner
-        fourth: false, // Control para mostrar resultados
-        /* ------------------ Variables para los AAs ------------------ */
-        aaIsDisabled: false, // Para desactivar los controles para seleccionar AA
-        aaSlideDisabled: false, // Para desactivar el slider unicamente
-        aaSequence: ["M"], // Secuencia de AA de la proteina (se carga con Axios al iniciar la pagina)
-        aaPostSequence: ["M"], // Secuencia de AA de la proteina post mutacion
-        // Objeto con AAs: llaves - letra, valor - nombre e imagen
+        third: false,
+        fourth: false,
+        isDisabled: false,
+        aaSequence: ["M","S","Y","Y","H","H","H","H","H","H","D","Y","D","I","P","T","T","E","N","L","Y","F","Q","G","A","M","G","I","L","G","S","G","Q","K","H","F","E","K","R","R","N","P","A","A","G","L","I","Q","S","A","W","R","F","Y","A","T","N","L","S","R","T","D","L","H","S","T","W","Q","Y","Y","E","R","T","V","T","V","P","M","Y","R","G","L","E","D","L","T","P","G","L","K","V","S","I","R","A","V","C","V","M","R","F","L","V","S","K","R","K","F","K","E","S","L","R","L","D"],
         aaMut: {"G" : ["Glycine", "glycine.png"],
                 "A" : ["Alanine", "alanine.png"],
                 "L" : ["Leucine", "leucine.png"],
@@ -36,107 +27,29 @@ const { createApp } = Vue
                 "D" : ["Aspartic Acid", "aspartic_acid.png"],
                 "T" : ["Threonine", "threonine.png"]
               },
-        aaImgs: {}, // Objeto para pre-cargar las imagenes
-        aaMutCode: "", // Codigo de la mutacion
-        aa: 0, // Posicion del AA a mutar
-        aaSel: "M", // Letra del AA que reemplazara
-        /* ------------------ Variables para el ADN ------------------ */
-        dnaIsDisabled: false, // Para desactivar los controles para seleccionar nucleotido
-        dnaSlideDisabled: false, // Para desactivar el slider unicamente
-        dnaSequence: ["A"], // Secuencia de ADN de la proteina (se carga con Axios al iniciar la pagina)
-        dnaPostSequence: ["A"], // Secuencia de ADN de la proteina post mutacion
-        // Objeto con nucleotidos: llaves - letra, valor - nombre e imagen
-        dnaMut: {"A" : ["Adenine", "adenine.png"],
-                "C" : ["Cytosine", "cytosine.png"],
-                "G" : ["Guanine", "guanine.png"],
-                "T" : ["Thymine", "thymine.png"]
-              },
-        dnaImgs: {}, // Objeto para pre-cargar las imagenes
-        dnaMutCode: "", // Codigo de la mutacion
-        dna: 0, // Posicion del nucleotido a mutar
-        dnaSel: "A" // Letra del nucleotido que reemplazara
+        image1: "glycine.png",
+        image2: "methionine.png",
+        mutCode: "",
+        aa: 0,
+        sel: "M"
       }
     },
     methods: {
-      /* Metodo para restablecer todo a su configuracion inicial */
-      reset() {
-        this.dnaIsDisabled = false;
-        this.dnaSlideDisabled = false;
-        this.aaIsDisabled = false;
-        this.aaSlideDisabled = false;
-        this.dnaPostSequence = [...this.dnaSequence];
-        this.aaPostSequence = [...this.aaSequence];
-        this.dnaMutCode = "";
-        this.aaMutCode = "";
-        this.dna = 0;
-        this.aa = 0;
-        this.dnaSel = "A";
-        this.aaSel = "M";
-      },
-      /* Metodo AA para actualizar el codigo de mutacion al mover el slider */
       updateAA() {
-        this.aaMutCode = this.aaSequence[this.aa] + (+this.aa + 1) + this.aaPostSequence[this.aa];
+        this.mutCode = this.aaSequence[this.aa] + (+this.aa + 1) + this.sel;
       },
-      /* Metodo DNA para actualizar el codigo de mutacion al mover el slider */
-      updateDNA() {
-        this.dnaMutCode = this.dnaSequence[this.dna] + (+this.dna + 1) + this.dnaPostSequence[this.dna];
-      },
-      /* Metodo para actualizar el codigo de mutacion al seleccionar el AA para reemplazar */
       onAA(nam) {
-        this.aaSel = nam;
-        this.aaMutCode = this.aaSequence[this.aa] + (+this.aa + 1) + nam;
-        this.aaPostSequence[this.aa] = nam;
-        // Check if both sequences are different
-        var difference = 0;
-        for (var i = 0; i < this.aaSequence.length; i++) {
-          if (this.aaSequence[i] != this.aaPostSequence[i]) {
-            difference = difference + 1;
-          }
-        }
-        if (difference != 0) {
-          this.aaSlideDisabled = true;
-        } else {
-          this.aaSlideDisabled = false;
-        }
+        this.image2 = this.aaMut[nam][1];
+        this.sel = nam;
+        //this.mutCode = this.aaSequence[this.aa] + (+this.aa + 1) + nam;
       },
-      /* Metodo para actualizar el codigo de mutacion al seleccionar el nucleotido para reemplazar */
-      onDNA(nam) {
-        this.dnaSel = nam;
-        this.dnaMutCode = this.dnaSequence[this.dna] + (+this.dna + 1) + nam;
-        this.dnaPostSequence[this.dna] = nam;
-        // Check if both sequences are different
-        var difference = 0;
-        for (var i = 0; i < this.dnaSequence.length; i++) {
-          if (this.dnaSequence[i] != this.dnaPostSequence[i]) {
-            difference = difference + 1;
-          }
-        }
-        if (difference != 0) {
-          this.dnaSlideDisabled = true;
-        } else {
-          this.dnaSlideDisabled = false;
-        }
-      },
-      /* Metodo para pedirle la evaluacion de la nueva secuencia segun el modelo al backend */
-      aaRun() {
-        if ((this.aaMutCode.length >= 3) && (this.aaMutCode.length <= 5)) {
-          var primera = this.aaMutCode.slice(0,1);
-          var ultima = this.aaMutCode.slice(-1);
-          if (Object.keys(this.aaMut).includes(primera) && Object.keys(this.aaMut).includes(ultima) && (primera != ultima)) {
-            var numero = this.aaMutCode.slice(1,-1);
-            if ((+numero >= 1) && (+numero <= this.aaSequence.length)) {
-              axios
-                .get(this.endpoint + "aamutation", {
-                  params: {
-                    code: this.aaMutCode
-                  }
-                })
-                .then(response => {
-                  console.log(response);
-                  this.third = false;
-                })
-              this.third = true;
-              this.aaIsDisabled = true;
+      run() {
+        if ((this.mutCode.length >= 3) && (this.mutCode.length <= 5)) {
+          var primera = this.mutCode.slice(0,1);
+          var ultima = this.mutCode.slice(-1);
+          if (Object.keys(this.aaMut).includes(primera) && Object.keys(this.aaMut).includes(ultima)) {
+            var numero = this.mutCode.slice(1,-2);
+            if ((+numero >= 1) && (+numero < this.aaSequence.length)) {
               console.log("Genial!")
             } else {
               console.log("Error de numero");
@@ -147,74 +60,8 @@ const { createApp } = Vue
         } else {
           console.log("Error de largo");
         }
-      },
-      /* Metodo para pedirle la evaluacion de la nueva secuencia segun el modelo al backend */
-      dnaRun() {
-        if ((this.dnaMutCode.length >= 3) && (this.dnaMutCode.length <= 5)) {
-          var primera = this.dnaMutCode.slice(0,1);
-          var ultima = this.dnaMutCode.slice(-1);
-          if (Object.keys(this.dnaMut).includes(primera) && Object.keys(this.dnaMut).includes(ultima) && (primera != ultima)) {
-            var numero = this.dnaMutCode.slice(1,-1);
-            if ((+numero >= 1) && (+numero <= this.dnaSequence.length)) {
-              axios
-                .get(this.endpoint + "dnamutation", {
-                  params: {
-                    code: this.dnaMutCode
-                  }
-                })
-                .then(response => {
-                  console.log(response);
-                  this.third = false;
-                })
-              this.third = true;
-              this.dnaIsDisabled = true;
-              console.log("Genial!")
-            } else {
-              console.log("Error de numero");
-            }
-          } else {
-            console.log("Error de letras");
-          }
-        } else {
-          console.log("Error de largo");
-        }
+        this.third = true;
+        this.isDisabled = true;
       }
-    },
-    /* !!!!! ANTES DE MONTAR VUE.JS EN LA PAGINA !!!!! */
-    beforeMount () {
-      /* Solicitar al backend la secuencia de AAs */
-      const getAASequence = async () => {
-        await axios
-          .get(this.endpoint + "aminoacid")
-          .then(response => {
-            this.aaSequence = response['data'].split("");
-            this.aaPostSequence = response['data'].split("");
-            this.primero = true;
-          });
-      }
-      /* Solicitar al backend la secuencia de ADN */
-      const getDNASequence = async () => {
-        await axios
-          .get(this.endpoint + "dna")
-          .then(response => {
-            this.dnaSequence = response['data'].split("");
-            this.dnaPostSequence = response['data'].split("");
-            this.segundo = true;
-          });
-      }
-      getAASequence();
-      getDNASequence();
-    },
-    /* !!!!! JUSTO DESPUES DE MONTAR VUE.JS EN LA PAGINA !!!!! */
-    mounted () {
-      /* Pre-cargar las imagenes en el cache del navegador para no hacer el proceso tan demandante */
-      Object.keys(this.aaMut).forEach(element => {
-        this.aaImgs[element] = new Image();
-        this.aaImgs[element].src = "IMG/" + this.aaMut[element][1];
-      });
-      Object.keys(this.dnaMut).forEach(element => {
-        this.dnaImgs[element] = new Image();
-        this.dnaImgs[element].src = "IMG/" + this.dnaMut[element][1];
-      });
     }
   }).mount('#app')
--- README.md
+++ README.md
@@ -1,20 +1,2 @@
-# MutaGen Kv7.2
-
-A free machine learning web service to predict pathogenicity of missense variants in Kv7.2 channels
----
-
-This repository contains both the frontend and the backend to the MutaGen program. The latter takes a punctual mutation selected by the user in the frontend, and evaluates it using machine learning models to predict its pathogenicity.
-
-The web application was developed for a summer internship at the Universidad del Pais Vasco. For more information, please contact: <some_user@some_domain.es>
-
-## Backend
-It's written in Python 3 and using several libraries for both the server and the machine learning models. To run the backend, please consider the following required libraries:
-
-* Flask
-* Jinja
-* Werkzeug
-* Flask-Cors
-* SciKit Learn
-
-## Frontend
-It's written in HTML5 using Bootstrap 5 for CSS and VueJS 3 and Axios for the JS part.
+# Mutants
+Punctual mutations
